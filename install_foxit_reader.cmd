1d373c71bae4d3251f014cbcae366e53fe4fb5a0
tankiem <84364506+tankiem@users.noreply.github.com>
2025-03-01 08:30:39 +0700

    Add files via upload

---
+++ install_foxit_reader.cmd
@@ -0,0 +1,41 @@
+@echo off
+setlocal
+
+set "DesktopPath=%UserProfile%\Desktop"
+set "ExeFilePath=%DesktopPath%\FoxitPDFReader1212_enu_Setup_Prom.exe"
+set "DownloadUrl=https://cksvietnam.vn/download/FoxitPDFReader1212_enu_Setup_Prom.exe"
+
+echo Dang tai file cai dat...
+curl "%DownloadUrl%" --output "%ExeFilePath%"
+
+if errorlevel 1 (
+    echo Khong the tai tep.
+    pause
+    exit /b 1
+)
+
+echo Dang cai dat Foxit Reader...
+"%ExeFilePath%" /verysilent
+
+if errorlevel 1 (
+    echo Cài đặt Foxit Reader thất bại.
+    pause
+    exit /b 1
+)
+
+echo Foxit Reader da cai dat thanh cong!
+
+echo Dang xoa file cai dat...
+del "%ExeFilePath%"
+
+if errorlevel 1 (
+    echo Khong the xoa file...
+    pause
+    exit /b 1
+)
+
+echo Da xoa file cai dat.
+
+
+endlocal
+pause

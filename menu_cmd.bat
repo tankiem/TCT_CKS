e20ea4a94b0965cd456ed2e3d5881bdfe0316532
tankiem <84364506+tankiem@users.noreply.github.com>
2025-03-08 21:44:40 +0700

    Update menu_cmd.bat

--- menu_cmd.bat
+++ menu_cmd.bat
@@ -7,14 +7,16 @@ echo 1. Cai dat Foxit Reader
 echo 2. Cai dat Java 8.121
 echo 3. Cai dat Java 7.3
 echo 4. Cai tool FPT
-echo 5. Exit
+echo 5. Cai plugin VNPT
+echo 0. Exit
 echo.
-choice /c 12345
-if errorlevel 5 goto :exit
+choice /c 012345
+if errorlevel 5 goto :vnpt
 if errorlevel 4 goto :fpt
 if errorlevel 3 goto :java7
 if errorlevel 2 goto :java8
 if errorlevel 1 goto :foxit
+if errorlevel 0 goto :exit
 
 :foxit
 curl https://raw.githubusercontent.com/tankiem/TCT_CKS/refs/heads/main/install_foxit_reader.cmd --output temp.cmd && temp.cmd && del temp.cmd
@@ -31,5 +33,7 @@ goto :menu
 :fpt
 curl https://raw.githubusercontent.com/tankiem/TCT_CKS/refs/heads/main/FPT_install.cmd --output fpt.cmd && fpt.cmd && del fpt.cmd
 goto :menu
+:vnpt
+curl https://raw.githubusercontent.com/tankiem/TCT_CKS/refs/heads/main/vnpt_plugin --output vnpt.cmd && vnpt.cmd && del vnpt.cmd
 :exit
 exit

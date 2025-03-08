4fb1b95215fcb2fedc81601d325667c85c00b4a6
tankiem <84364506+tankiem@users.noreply.github.com>
2025-03-08 22:30:05 +0700

    Update menu_cmd.bat

--- menu_cmd.bat
+++ menu_cmd.bat
@@ -8,9 +8,11 @@ echo 2. Cai dat Java 8.121
 echo 3. Cai dat Java 7.3
 echo 4. Cai tool FPT
 echo 5. Cai plugin VNPT
+echo 6. Cai tool ky so BHXH free
 
 echo.
-choice /c 12345
+choice /c 123456
+if errorlevel 6 goto :bhxh_free
 if errorlevel 5 goto :vnpt
 if errorlevel 4 goto :fpt
 if errorlevel 3 goto :java7
@@ -37,5 +39,9 @@ goto :menu
 :vnpt
 curl https://raw.githubusercontent.com/tankiem/TCT_CKS/refs/heads/main/vnpt_plugin --output vnpt.cmd && vnpt.cmd && del vnpt.cmd
 goto :menu
+:bhxh_free
+curl https://raw.githubusercontent.com/tankiem/TCT_CKS/refs/heads/main/tool_ky_bhxh_mienphi --output bhxh.cmd && bhxh.cmd && del bhxh.cmd
+goto :menu
+
 :exit
 exit

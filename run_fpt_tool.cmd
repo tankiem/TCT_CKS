317b427fe06a9c8d79bb33206e87a55e0a680955
tankiem <84364506+tankiem@users.noreply.github.com>
2025-04-17 00:36:35 +0700

    Update run_fpt_tool.cmd

--- run_fpt_tool.cmd
+++ run_fpt_tool.cmd
@@ -2,16 +2,15 @@
 set "SCRIPT_URL=https://raw.githubusercontent.com/tankiem/TCT_CKS/refs/heads/main/fpt_tool.ps1"
 set "TEMP_PS1=%TEMP%pt_tool_online.ps1"
 
-:: Kiểm tra xem script đã được chạy dưới quyền admin chưa
-net session >nul 2>&1
+:: Kiểm tra PowerShell
+where powershell >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Chay lai script voi quyen admin...
-    :: Dùng PowerShell để khởi chạy lại script dưới quyền admin
-    powershell -Command "Start-Process cmd.exe -ArgumentList '/c %~dp0run_fpt_tool_with_admin.cmd' -Verb RunAs"
-    exit
+    echo PowerShell không tồn tại. Cài PowerShell để tiếp tục.
+    pause
+    exit /b
 )
 
-:: Nếu đã có quyền admin, tải script từ GitHub và chạy
+:: Tải file PowerShell từ GitHub
 echo Dang tai script tu GitHub...
 powershell -Command "Invoke-WebRequest -Uri '%SCRIPT_URL%' -OutFile '%TEMP_PS1%'"
 

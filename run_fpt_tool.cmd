0009dba8dc72c38cf344432263b040eabd6e6779
tankiem <84364506+tankiem@users.noreply.github.com>
2025-04-17 00:14:51 +0700

    Update run_fpt_tool.cmd

--- run_fpt_tool.cmd
+++ run_fpt_tool.cmd
@@ -1,15 +1,24 @@
 @echo off
-set "SCRIPT_URL=fpt_tool.ps1"
-set "TEMP_PS1=%~dp0%SCRIPT_URL%"
+set "SCRIPT_URL=https://raw.githubusercontent.com/tankiem/TCT_CKS/refs/heads/main/fpt_tool.ps1"
+set "TEMP_PS1=%TEMP%\fpt_tool_online.ps1"
 
 :: Kiểm tra PowerShell
 where powershell >nul 2>&1
 if %errorlevel% neq 0 (
-    echo PowerShell không tồn tại. Vui lòng cài PowerShell 2.0 trở lên.
+    echo PowerShell không tồn tại. Cài PowerShell để tiếp tục.
     pause
     exit /b
 )
 
-:: Chạy PowerShell script
-powershell -ExecutionPolicy Bypass -File "%TEMP_PS1%"
+:: Tải file PowerShell từ GitHub
+echo Dang tai script tu GitHub...
+powershell -Command "Invoke-WebRequest -Uri '%SCRIPT_URL%' -OutFile '%TEMP_PS1%'"
+
+:: Kiểm tra và chạy
+if exist "%TEMP_PS1%" (
+    powershell -ExecutionPolicy Bypass -File "%TEMP_PS1%"
+) else (
+    echo Khong tai duoc script. Kiem tra mang hoac link sai.
+)
+
 pause

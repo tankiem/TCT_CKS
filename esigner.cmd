51670153a2f6fb164e276b171cc2a9adfb425ad5
tankiem <84364506+tankiem@users.noreply.github.com>
2026-03-26 07:35:24 +0700

    Update esigner.cmd

--- esigner.cmd
+++ esigner.cmd
@@ -1,52 +1,45 @@
 @echo off
-setlocal enabledelayedexpansion
 
-set WORKDIR=%USERPROFILE%\Desktop\eSigner_setup
+set WORKDIR=%TEMP%\eSigner
 set ZIPFILE=%WORKDIR%\eSigner.zip
 set URL=https://cksvietnam.vn/download/eSigner_1.1.0_setup.zip
 
-if exist "%WORKDIR%" rd /s /q "%WORKDIR%"
+rd /s /q "%WORKDIR%" >nul 2>&1
 mkdir "%WORKDIR%"
 
 echo ==== eSigner AUTO INSTALL ====
 
-echo Dang tai file...
-powershell -Command "$wc=New-Object Net.WebClient;$wc.DownloadFile('%URL%','%ZIPFILE%')"
+echo Dang tai...
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%URL%','%ZIPFILE%')"
 
-if not exist "%ZIPFILE%" (
- echo Loi tai file
- pause
- exit /b 1
-)
+if not exist "%ZIPFILE%" echo Loi tai file & pause & exit /b
 
 echo Dang giai nen...
 powershell -Command "Expand-Archive '%ZIPFILE%' '%WORKDIR%' -Force"
 
-echo Dang tim file cai dat...
+echo Dang tim file setup...
 
-set MaxSize=0
-set ExePath=
-
-for /r "%WORKDIR%" %%i in (*.exe) do (
- set size=%%~zi
- if !size! GTR !MaxSize! (
-  set MaxSize=!size!
-  set ExePath=%%i
- )
-)
+for /r "%WORKDIR%" %%i in (*.exe) do call :check "%%i"
 
 if not defined ExePath (
- echo Khong tim thay exe
+ echo Khong tim thay file exe
  pause
- exit /b 1
+ exit /b
 )
 
-echo File duoc chon: %ExePath%
-echo Size: %MaxSize%
-
-echo Dang cai dat...
-
+echo Chay file: %ExePath%
 start /wait "" "%ExePath%" /SILENT
 
 echo Hoan tat!
 pause
+exit /b
+
+:check
+set file=%~1
+for %%A in ("%file%") do set size=%%~zA
+if not defined MaxSize set MaxSize=0
+if %size% GTR %MaxSize% (
+ set MaxSize=%size%
+ set ExePath=%file%
+)
+exit /b
